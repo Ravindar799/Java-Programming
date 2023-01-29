c30adf1d48c369e60ee2d37012d62ae9a2e017c5
Ravindar799 <111416859+Ravindar799@users.noreply.github.com>
2023-01-29 03:44:58 -0800

    Update Main.java

--- Oops-Java/Main.java
+++ Oops-Java/Main.java
@@ -20,18 +20,18 @@ class Student {
  public class Main {
      public static void main (String arr[]) {
         Student s = new Student();
-        s.name = "ravindar";
-        s.studentId = "xyz";
-       s.printName();
-       s.printStudentId();
+           s.name = "ravindar";
+           s.studentId = "xyz";
+           s.printName();
+           s.printStudentId();
        Student s1 = new Student("rahul","zyx");
-       s1.printName();
-       s1.printStudentId();
+           s1.printName();
+           s1.printStudentId();
        Student student1 = new Student("ravi","std1");
        Student student2 = new Student("ravi","std1");
-       int hashValue1 = student1.hashCode();
+           int hashValue1 = student1.hashCode();
        System.out.println("hashcode of student1 is:"+hashValue1);
-       int hashValue2 = student2.hashCode();
+          int hashValue2 = student2.hashCode();
        System.out.println("hashcode of student1 is:"+hashValue2);
        if(student1 == student2) {
          System.out.println("two objects are same");
