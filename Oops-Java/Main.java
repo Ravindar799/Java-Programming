38220aaa12cca34322cdd3f1be4e982b1568d365
unknown <ravindardeeti799@gmail.com>
2023-01-30 07:36:58 -0800

    modified version

--- Oops-Java/Main.java
+++ Oops-Java/Main.java
@@ -1,43 +1,16 @@
-class Student {
-    String name;
-    String studentId;
-    Student() {
-
+ class Person {
+    private String name;
+    public String getName() {
+        return name;
     }
-    Student(String name, String studentId) {
+    public void setName(String name) {
         this.name = name;
-        this.studentId = studentId;
     }
-
-    public void printName() {
-        System.out.println(name);
-    }
-
-    public void printStudentId () {
-        System.out.println(studentId);
-    }  
 }
- public class Main {
-     public static void main (String arr[]) {
-        Student s = new Student();
-        s.name = "ravindar";
-        s.studentId = "xyz";
-       s.printName();
-       s.printStudentId();
-       Student s1 = new Student("rahul","zyx");
-       s1.printName();
-       s1.printStudentId();
-       Student student1 = new Student("ravi","std1");
-       Student student2 = new Student("ravi","std1");
-       int hashValue1 = student1.hashCode();
-       System.out.println("hashcode of student1 is:"+hashValue1);
-       int hashValue2 = student2.hashCode();
-       System.out.println("hashcode of student1 is:"+hashValue2);
-       if(student1 == student2) {
-         System.out.println("two objects are same");
-        } 
-        else  {
-         System.out.println("two objects are not same");
-        }
+public class  Main {
+    public static void main (String arr[]){
+        Person p = new Person();
+        p.setName("ravindar");
+        System.out.println(p.getName());
     }
 }
